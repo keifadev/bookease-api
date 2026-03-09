27f828e9122f5a13b127866e3568e1015a44558c
keifadev <joaojosafa11@gmail.com>
2026-03-09 02:45:04 +0000

    chore: initial project setup with Spring Boot, Docker Compose and configs

---
+++ src/main/java/com/keifa/bookease/BookeaseApplication.java
@@ -0,0 +1,13 @@
+package com.keifa.bookease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keifa/bookease/BookeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keifa.bookease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
