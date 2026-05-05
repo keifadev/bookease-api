cb215c01f28d74220540e5020dd01f0f07c51413
keifadev <joaojosafa11@gmail.com>
2026-05-05 21:48:55 +0000

    feat: add OfferedService domain exceptions

---
+++ src/main/java/com/keifa/bookease/catolog/exception/InvalidServiceDurationException.java
@@ -0,0 +1,7 @@
+package com.keifa.bookease.catolog.exception;
+
+public class InvalidServiceDurationException extends RuntimeException {
+    public InvalidServiceDurationException(String message) {
+        super(message);
+    }
+}
