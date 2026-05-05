68f96603b3269780eeba89e296719262858f4a1a
keifadev <joaojosafa11@gmail.com>
2026-05-05 21:03:43 +0000

    feat: add Specialty and SpecialtyCategory enums

---
+++ src/main/java/com/keifa/bookease/professional/enums/SpecialtyCategory.java
@@ -0,0 +1,11 @@
+package com.keifa.bookease.professional.enums;
+
+public enum SpecialtyCategory {
+    HEALTH,
+    BEAUTY,
+    FITNESS,
+    EDUCATION,
+    PET_CARE,
+    TECHNOLOGY,
+    OTHER
+}
