93123b77587ce9696339996fb844ca434af97ac5
keifadev <joaojosafa11@gmail.com>
2026-05-08 15:27:20 +0000

    refactor: rename OwnershipValidator params for clarity

--- src/main/java/com/keifa/bookease/common/util/OwnershipValidator.java
+++ src/main/java/com/keifa/bookease/common/util/OwnershipValidator.java
@@ -1,15 +1,14 @@
 package com.keifa.bookease.common.util;
 
 import com.keifa.bookease.common.exception.UnauthorizedAccessException;
-import org.springframework.lang.NonNull;
 
 public class OwnershipValidator {
     private OwnershipValidator() {
     }
 
-    public static void validateOwnership(Object owner, Object entity) {
-        if (!owner.equals(entity)) {
-            throw new UnauthorizedAccessException("You are not authorized to access this resource");
+    public static void validateOwnership(Object resourceOwnerId, Object requesterId) {
+        if (!resourceOwnerId.equals(requesterId)) {
+            throw new UnauthorizedAccessException("Você não tem permissão para acessar este recurso");
         }
     }
 }
